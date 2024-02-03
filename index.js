769c5dd191777e377e0844de0154ff53b26c1210
fangqijun <zfangqj@gmail.com>
2024-02-04 05:15:04 +0800

    Update config file path

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const config = JSON.parse(
-    await readFile(path.resolve(__dirname, './config.json'))
+    await readFile(path.resolve(__dirname, './config.local.json'))
 );
 
 
